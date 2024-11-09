e367f46dbc1562b77aa24cb6454254e5f9679a60
Tim Cacayan <tim.cacayan@coralactive.com>
2024-11-09 16:27:28 +0800

    Added Test Plan in the docs directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 1,
   workers: process.env.CI ? 1 : '50%',
   use: {
-    headless: process.env.CI ? true : false,
+    headless: false,
     baseURL: 'https://app.staging.salesbricks.com/products/platform/new?sku=d4fedad5-4c48-5a6d-b6e6-0381a6490757',
     trace: 'retain-on-failure',
   },
